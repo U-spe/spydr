58620a8fabc459f42f7a1bcc8d6de7b4a843d772
Web Creation Studios (WCS) <solutionsforyourweb@gmail.com>
2026-07-03 14:52:17 -0400

    Update favicon handling in CloakManager

--- js/cloak.js
+++ js/cloak.js
@@ -1,21 +1,36 @@
-// /js/cloak.js
+// js/cloak.js
 export default class CloakManager {
     constructor(kernel) {
         this.kernel = kernel;
         this.presets = {
-            'google-docs': {
-                title: 'Google Docs',
-                favicon: 'https://ssl.gstatic.com/docs/documents/images/kix-favicon-7.ico'
+            'spydr': {
+                title: "spydr",
+                icon: "assets/images.png"
             },
-            'google-drive': {
-                title: 'My Drive - Google Drive',
-                favicon: 'https://ssl.gstatic.com/images/branding/product/1x/drive_2020q4_32dp.png'
+            'google': {
+                title: "Google",
+                icon: "https://www.google.com/favicon.ico"
             },
-            'canvas': {
-                title: 'Dashboard',
-                favicon: 'https://du11hjcvx0uqb.cloudfront.net/dist/images/favicon-e05d51a14c.ico'
+            'classroom': {
+                title: "Home - Classroom",
+                icon: "https://ssl.gstatic.com/classroom/favicon.png"
+            },
+            'securly': {
+                title: "Create Pass - Securly Pass",
+                icon: "https://pass.securly.com/favicon.ico"
+            },
+            'wayground': {
+                title: "Playing a Game - Wayground (Formerly Quizizz)",
+                icon: "https://wayground.com/favicon.ico"
+            },
+            'studysync': {
+                title: "StudySync",
+                icon: "https://www.studysync.com/static/favicon.ico"
+            },
+            'classlink': {
+                title: "My Apps - Classlink",
+                icon: "https://lh3.googleusercontent.com/eZN3KtQ96nLtnBk6vNJWNe8lAGQ_v4fWGTH7URUqzMyQ9BiDuxqpt1uSkIU-OaRhxw"
             }
-           
         };
         this.originalTitle = document.title;
         this.originalFavicon = '';
@@ -37,13 +52,13 @@ export default class CloakManager {
         const targetKey = this.settings.get('cloakTarget');
 
         if (enabled && this.presets[targetKey]) {
-            this.applySpoof(this.presets[targetKey].title, this.presets[targetKey].favicon);
+            this.applySpoof(this.presets[targetKey].title, this.presets[targetKey].icon);
         } else {
             this.restoreSpoof();
         }
     }
 
-    applySpoof(title, faviconUrl) {
+    applySpoof(title, iconUrl) {
         document.title = title;
         let link = document.querySelector("link[rel~='icon']");
         if (!link) {
@@ -51,7 +66,7 @@ export default class CloakManager {
             link.rel = 'icon';
             document.head.appendChild(link);
         }
-        link.href = faviconUrl;
+        link.href = iconUrl;
     }
 
     restoreSpoof() {

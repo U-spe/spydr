9a883a81abe1538188e007a5a7c3089744e9685b
Web Creation Studios (WCS) <solutionsforyourweb@gmail.com>
2026-07-14 15:28:20 -0400

    Update Supabase URL to remove endpoint path

--- js/chat.js
+++ js/chat.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const supabaseUrl = "https://bxsiniqhswmiokqshcgi.supabase.co/rest/v1/";
+const supabaseUrl = "https://bxsiniqhswmiokqshcgi.supabase.co";
 const supabaseKey = "sb_publishable_UHN349V0ftl3nJVle0za2Q_oWeHWMvd"; 
 const supabase = createClient(supabaseUrl, supabaseKey);
 

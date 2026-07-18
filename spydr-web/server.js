88509ce701cdcbbd04cae8d81b2675002dc151f9
123456 <a123456@123456s-iMac.local>
2026-07-18 17:20:07 -0400

    add scramjet bare server and proxy setup

--- spydr-web/server.js
+++ spydr-web/server.js
@@ -1,5 +1,6 @@
-const { createBareServer } = require("@tomphttp/bare-server-node");
 const express = require("express");
+const path = require("path");
+const { createBareServer } = require("@tomphttp/bare-server-node");
 
 const app = express();
 
@@ -15,12 +16,19 @@ app.use((req, res, next) => {
     }
 });
 
-app.get("*", (req, res) => {
-    res.sendFile(__dirname + "/public/sp1dr.html");
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "sp1dr.html"));
 });
 
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(port, () => {
-    console.log(`Spydr running on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`spydr running on ${PORT}`);
 });
+
+app.use(express.static("public"));
+
+app.use((req, res, next) => {
+    if (bare.shouldRoute(req)) {
+        bare.routeRequest(req, res);
+    } else 

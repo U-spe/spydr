444e2cb113fcfd53e9e8f236b8294f16a64be330
Web Creation Studios (WCS) <solutionsforyourweb@gmail.com>
2026-07-18 14:26:17 -0400

    Create sp1dr.js

---
+++ spydr-web/public/sp1dr.js
@@ -0,0 +1,10 @@
+const input = document.getElementById("url");
+const button = document.getElementById("go");
+
+button.onclick = () => {
+
+    const query = input.value.trim();
+
+    console.log(query);
+
+};

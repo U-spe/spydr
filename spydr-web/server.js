cb14619749518f11c02e43bf0c233f9372152e9c
Web Creation Studios (WCS) <solutionsforyourweb@gmail.com>
2026-07-18 17:03:09 -0400

    Refactor server to use bare-server and update port

--- spydr-web/server.js
+++ spydr-web/server.js
@@ -1,16 +1,26 @@
+const { createBareServer } = require("@tomphttp/bare-server-node");
 const express = require("express");
-const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, "public")));
-app.use("/scram", express.static(path.join(__dirname, "scram")));
+const bare = createBareServer("/bare/");
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "sp1dr.html"));
+app.use(express.static("public"));
+
+app.use((req, res, next) => {
+    if (bare.shouldRoute(req)) {
+        bare.routeRequest(req, res);
+    } else {
+        next();
+    }
 });
 
-app.listen(PORT, () => {
-    console.log(`spydr running on http://localhost:${PORT}`);
+app.get("*", (req, res) => {
+    res.sendFile(__dirname + "/public/sp1dr.html");
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Spydr running on port ${port}`);
 });
